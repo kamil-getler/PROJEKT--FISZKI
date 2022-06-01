231f8b873e477ada9abe905e18ced2d71b6b25ca
KamilGetler <kamilgetler@gmail.com>
2022-06-01 15:38:28 +0200

    New

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://mongodb1247:mongodb1247@cluster0.zztka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000
+}
---
+++ routes/news.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const News = require('../models/news');
+const Learn = require('../models/learn');
+
+/* GET home page. */
+router.get('/', (req, res) => {
+    const search = req.query.search || '';
+
+    const findNews = News
+        .find({ title: new RegExp(search.trim(), 'i') })
+        .sort({ created: -1 });
+
+    findNews.exec((err, data) => {
+        res.render('news', { title: 'News', data, search });
+    });
+});
+
+
+
+
+
+module.exports = router;
